3ea98f6f68ea94f5d76ad6942df54b92cd6e2309
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-19 19:21:27 +0200

    corfu: Import WiFi configs from stock

--- device.mk
+++ device.mk
@@ -91,6 +91,11 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy
 
