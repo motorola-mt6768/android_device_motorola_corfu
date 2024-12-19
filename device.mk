56734924ccf840fb51db1536c9d467dbb7393956
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-19 09:29:35 -0500

    corfu: Build fastbootd

--- device.mk
+++ device.mk
@@ -65,3 +65,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
