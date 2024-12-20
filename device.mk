00217ad505c88d900e57681ee3de5fa3d744c42b
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-19 22:17:22 -0500

    corfu: Inherit 4GB Dalvik heap configuration

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
