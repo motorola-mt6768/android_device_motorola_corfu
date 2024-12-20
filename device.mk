4bdacf4db0e78946643c601a73e8e389d54567c7
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-19 22:38:22 -0500

    corfu: Build ndk_platform variant of android.hardware.power-V2

--- device.mk
+++ device.mk
@@ -352,6 +352,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-V2-ndk_platform.vendor \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
