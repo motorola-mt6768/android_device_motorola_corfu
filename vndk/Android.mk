7306ffbf9caeed0f89c770b52be585930de57755
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-28 06:25:50 -0500

    corfu: upgrade to LOS 21

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -14,8 +14,9 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libhidlbase-v32
-LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so
+LOCAL_MODULE := libcrypto-v32
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TARGET_ARCH := arm64
@@ -25,13 +26,26 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libui-v32
-LOCAL_SRC_FILES := v32/arm64/arch-arm-armv8-a/shared/vndk-core/libui.so
+LOCAL_MODULE := libssl-v32
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libssl.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TARGET_ARCH := arm
+LOCAL_MODULE_TARGET_ARCH := arm64
 LOCAL_MODULE_TAGS := optional
 LOCAL_CHECK_ELF_FILES := false
 LOCAL_VENDOR_MODULE := true
-LOCAL_REQUIRED_MODULES := android.hardware.graphics.common-V2-ndk_platform
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libstagefright_foundation-v33
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_arm := v33/arm64/arch-arm-armv8-a/shared/vndk-core/libstagefright_foundation.so
+LOCAL_SRC_FILES_arm64 := v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+LOCAL_MODULE_TAGS := optional
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
