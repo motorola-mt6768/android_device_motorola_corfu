820875f965176422f289fc966e23a119fc4c5d72
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-29 08:03:37 -0500

    corfu: correct

--- device.mk
+++ device.mk
@@ -194,11 +194,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     mediatek-common \
     mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
+    mediatek-ims-oem-plugin \
+    mediatek-services 
 
 PRODUCT_PACKAGES += \
     libshim_sink
